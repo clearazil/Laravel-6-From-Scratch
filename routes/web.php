025c8211f0ccf9311682ea126eb484a11db5bacd
dvanderh <derkvanderheide@hotmail.com>
2020-02-19 15:49:58 +0100

    postscontroller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,3 +16,5 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/posts/{post}', 'PostsController@show');
