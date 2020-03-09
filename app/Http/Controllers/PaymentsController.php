5d88f6b5cd02d926ec7680563f98b1a8ab24aa50
Derk van der Heide <derkvanderheide@hotmail.com>
2020-03-09 15:47:20 +0100

    add events

--- app/Http/Controllers/PaymentsController.php
+++ app/Http/Controllers/PaymentsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\ProductPurchased;
 use App\Notifications\PaymentReceived;
 
 class PaymentsController extends Controller
@@ -14,6 +15,8 @@ class PaymentsController extends Controller
     public function store()
     {
         request()->user()->notify(new PaymentReceived(900));
+
+        ProductPurchased::dispatch('toy');
 
         return redirect(route('payments.create'))
             ->with('message', 'Payment made!');
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use App\Events\ProductPurchased;
+use App\Listeners\AwardAchievements;
+use App\Listeners\SendShareableCoupon;
 use Illuminate\Auth\Events\Registered;
 use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 use Illuminate\Foundation\Support\Providers\EventServiceProvider as ServiceProvider;
@@ -30,5 +33,10 @@ class EventServiceProvider extends ServiceProvider
         parent::boot();
 
         //
+    }
+
+    public function shouldDiscoverEvents()
+    {
+        return true;
     }
 }
